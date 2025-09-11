cd9f0d2b2482fc02ccfc90c951b5b7c0895f2834
Sidhant Thole <sidhantthole@coexistai.local>
2025-09-11 06:05:12 +0530

    updated quick setup for trying curl

--- quick_setup.sh
+++ quick_setup.sh
@@ -56,21 +56,26 @@ pip install -r ./requirements.txt
 
 # Adding tts files
 # Check if wget is installed
-if ! command -v wget &> /dev/null; then
-  echo "wget could not be found, please install wget to continue."
+# Check if wget or curl is installed
+if command -v wget &> /dev/null; then
+  DOWNLOADER="wget -O"
+elif command -v curl &> /dev/null; then
+  DOWNLOADER="curl -L -o"
+else
+  echo "Neither wget nor curl could be found, please install one to continue."
   exit 1
 fi
 
 # Download kokoro-v1.0.onnx if not present
 if [ ! -f kokoro-v1.0.onnx ]; then
-  wget https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/kokoro-v1.0.onnx
+  $DOWNLOADER kokoro-v1.0.onnx https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/kokoro-v1.0.onnx
 else
   echo "kokoro-v1.0.onnx already exists, skipping download."
 fi
 
 # Download voices-v1.0.bin if not present
 if [ ! -f voices-v1.0.bin ]; then
-  wget https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/voices-v1.0.bin
+  $DOWNLOADER voices-v1.0.bin https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/voices-v1.0.bin
 else
   echo "voices-v1.0.bin already exists, skipping download."
 fi
